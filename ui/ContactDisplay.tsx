2c8d8dfaec21aa7405561bf251f0768d5c7325b7
Katsuji <katsuji@paon.dev>
2023-11-07 10:39:44 +0900

    fix:classname

--- ui/ContactDisplay.tsx
+++ ui/ContactDisplay.tsx
@@ -11,7 +11,7 @@ type Props = {
 export function ContactDisplay(props: Props) {
   return (
     <Link className="flex items-center w-full lg:w-1/4 h-full p-5 bg-gray-50 dark:bg-slate-600 duration-300 shadow-xl gap-3 lg:hover:bg-sky-50 hover:shadow-3xl lg:hover:dark:bg-slate-500" href={props.href}>
-      <div className=''>
+      <div>
         {props.children}
       </div>
       <div className='text-gray-800 dark:text-slate-50 text-2xl pt-2 text-left'>
