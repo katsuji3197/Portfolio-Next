e84e54698f5cae07e9e61c2556d75e0a2aa5061c
Katsuji <katsuji@paon.dev>
2023-11-10 10:13:41 +0900

    GAでエラーが出ないように修正

--- app/works/poster/page.tsx
+++ app/works/poster/page.tsx
@@ -1,3 +1,4 @@
+import { GA } from '@/ui/GA'
 import { InfoDisplay } from '@/ui/InfoDisplay'
 import { ReadMoreButton } from '@/ui/ReadMoreButton'
 import { SectionTitle } from '@/ui/SectionTitle'
@@ -9,6 +10,7 @@ import Link from 'next/link'
 export default function Poster() {
   return (
     <main className="min-h-screen bg-cyan-50 dark:bg-slate-500">
+      <GA/>
       <div className='md:flex justify-start md:px-24 lg:px-40 xl:px-[300px] pt-64'>
         <SectionTitle title='Poster'>成果ポスター</SectionTitle>
       </div>
---
+++ ui/GA.tsx
@@ -0,0 +1,22 @@
+"use client";
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || '';
+
+export function GA() {
+  return (
+    <>
+    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${GA_TRACKING_ID}, {
+            page_path: window.location.pathname,
+          });
+      `,
+      }}
+      />
+      </>
+  );
+}
