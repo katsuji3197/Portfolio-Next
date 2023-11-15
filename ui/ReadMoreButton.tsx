30751e83527b7defb534bad0d3caecb0e56f0096
Katsuji <katsuji@paon.dev>
2023-11-15 16:32:11 +0900

    ReadMoreButtonのデザインを変更

--- ui/ReadMoreButton.tsx
+++ ui/ReadMoreButton.tsx
@@ -8,7 +8,7 @@ type Props = {
 export function ReadMoreButton(props: Props) {
   return (
     <div className='flex flex-wrap flex-col justify-center items-center pb-3 md:pb-5 pt-3'>
-      <Link href={props.href} className='flex-col justify-center text-center text-base md:text-xl text-gray-800 dark:text-white border border-gray-900 dark:border-white rounded-full px-5 md:px-8 py-3 md:py-4 hover:border-gray-400 hover:dark:border-gray-200 hover:text-gray-400 hover:dark:text-gray-200 duration-500 hover:px-10'>
+      <Link href={props.href} className='shadow-xl bg-white dark:bg-slate-700 flex-col justify-center text-center text-base md:text-xl text-gray-800 dark:text-white rounded-full px-5 md:px-8 py-3 md:py-4 hover:border-gray-400 hover:dark:border-gray-200 hover:text-gray-400 hover:dark:text-gray-200 duration-500 hover:px-10'>
           Read More
       </Link>
     </div>
