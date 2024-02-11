8cba4e7289c69e682c2fc0f887c944d4040cab13
Katsuji <katsuji@paon.dev>
2024-02-12 03:09:27 +0900

    ボタン感をなくす

--- ui/InfoDisplay.tsx
+++ ui/InfoDisplay.tsx
@@ -7,12 +7,14 @@ type Props = {
 
 export function InfoDisplay(props: Props) {
   return (
-    <div className="flex flex-col items-center w-full lg:w-1/4 h-full p-5 text-center duration-300">
-      <div className='text-gray-600 dark:text-slate-200 px-5'>
-        {props.title}
-      </div>
-      <div className='text-gray-800 dark:text-slate-50 text-xl sm:text-2xl pt-2'>
-        {props.children}
+    <div className='w-full lg:w-1/4 bg-blue-100 dark:bg-slate-700 px-1'>
+      <div className="flex flex-col items-center  h-full p-5 text-center duration-300 bg-gray-50 dark:bg-slate-600">
+        <div className='text-gray-600 dark:text-slate-200 px-5'>
+          {props.title}
+        </div>
+        <div className='text-gray-800 dark:text-slate-50 text-xl sm:text-2xl mt-2 font-medium'>
+          {props.children}
+        </div>
       </div>
     </div>
   );
